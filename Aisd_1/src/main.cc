1f078652a8b0b0c724fdb2362414439e26613eaa
Annaprogrammer300 <star2004.as@gmail.com>
2023-11-27 16:13:48 +0400

    start2.0

---
+++ Aisd_1/src/main.cc
@@ -0,0 +1,17 @@
+#include "C:\Users\user\Desktop\Aisd_lab\Aisd_1\include\matrix.h"
+
+
+
+
+int main()
+{ // Пример использования
+
+    Matrix<int> mat(3, 3, 1, 5);
+
+    std::cout << "Matrix A:\n";
+    mat.Print();
+    std::cout << std::endl;
+  
+
+    return 0;
+}
